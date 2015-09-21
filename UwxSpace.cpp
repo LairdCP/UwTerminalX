309c49820d69a2bb8b4cd8072b3bb061284091dd
thedjnK <spam@helper3000.net>
2015-09-21 08:30:57 +0100

    Add file headers

--- UwxSpace.cpp
+++ UwxSpace.cpp
@@ -1,8 +1,35 @@
+/******************************************************************************
+** Copyright (C) 2015 Laird
+**
+** Project: UwTerminalX
+**
+** Module: UwxMainWindow.cpp
+**
+** Notes:
+**
+** License: This program is free software: you can redistribute it and/or
+**          modify it under the terms of the GNU General Public License as
+**          published by the Free Software Foundation, version 3.
+**
+**          This program is distributed in the hope that it will be useful,
+**          but WITHOUT ANY WARRANTY; without even the implied warranty of
+**          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**          GNU General Public License for more details.
+**
+**          You should have received a copy of the GNU General Public License
+**          along with this program.  If not, see http://www.gnu.org/licenses/
+**
+*******************************************************************************/
+
+/******************************************************************************/
+// Include Files
+/******************************************************************************/
 #include "UwxSpace.h"
 #include "ui_UwxSpace.h"
 
-//=============================================================================
-//=============================================================================
+/******************************************************************************/
+// Local Functions or Private Members
+/******************************************************************************/
 UwxSpace::UwxSpace(QWidget *parent) : QDialog(parent), ui(new Ui::UwxSpace)
 {
     ui->setupUi(this);
@@ -62,3 +89,7 @@ UwxSpace::SetupMode(
         ui->label_Type->setText("Data segment");
     }
 }
+
+/******************************************************************************/
+// END OF FILE
+/******************************************************************************/
--- UwxSpace.h
+++ UwxSpace.h
@@ -1,13 +1,44 @@
+/******************************************************************************
+** Copyright (C) 2015 Laird
+**
+** Project: UwTerminalX
+**
+** Module: UwxPopup.h
+**
+** Notes:
+**
+** License: This program is free software: you can redistribute it and/or
+**          modify it under the terms of the GNU General Public License as
+**          published by the Free Software Foundation, version 3.
+**
+**          This program is distributed in the hope that it will be useful,
+**          but WITHOUT ANY WARRANTY; without even the implied warranty of
+**          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**          GNU General Public License for more details.
+**
+**          You should have received a copy of the GNU General Public License
+**          along with this program.  If not, see http://www.gnu.org/licenses/
+**
+*******************************************************************************/
 #ifndef UWXSPACE_H
 #define UWXSPACE_H
 
+/******************************************************************************/
+// Include Files
+/******************************************************************************/
 #include <QDialog>
 
+/******************************************************************************/
+// Forward declaration of Class, Struct & Unions
+/******************************************************************************/
 namespace Ui
 {
     class UwxSpace;
 }
 
+/******************************************************************************/
+// Class definitions
+/******************************************************************************/
 class UwxSpace : public QDialog
 {
     Q_OBJECT
@@ -16,6 +47,7 @@ public:
     explicit UwxSpace(
         QWidget *parent = 0
         );
+
     ~UwxSpace(
         );
 
@@ -42,3 +74,7 @@ private:
 };
 
 #endif // UWXSPACE_H
+
+/******************************************************************************/
+// END OF FILE
+/******************************************************************************/
